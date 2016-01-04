abd5def8a6c50af133635c8033c7d4360c46dc12
Chang SUN <sunchang0124@gmail.com>
2016-01-04 20:10:19 +0100

    include

--- code/src/UDoMLDP.cpp
+++ code/src/UDoMLDP.cpp
@@ -32,6 +32,19 @@
 #include <iostream>
 #include "featureExtraction/umHOG.h"
 
+#include "cv.h"
+#include "highgui.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+
 
 using namespace cv;
 
